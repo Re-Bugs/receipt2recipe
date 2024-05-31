54474d89cb7656ec6a7dd48dc222bfec7c9dd567
Lee SeungWook <leesw_0924@icloud.com>
2024-05-31 16:20:35 +0900

    CORS 설정 변경

--- SpringBoot/src/main/java/com/receipt2recipe/r2r/config/WebConfig.java
+++ SpringBoot/src/main/java/com/receipt2recipe/r2r/config/WebConfig.java
@@ -1,28 +1,30 @@
 package com.receipt2recipe.r2r.config;
 
 import com.receipt2recipe.r2r.interceptor.LoginInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@RequiredArgsConstructor
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
-    private LoginInterceptor loginInterceptor;
+    private final LoginInterceptor loginInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**") // 모든 경로에 대해 인터셉터를 적용
-                .excludePathPatterns("/sign_in", "/sign_up", "/resources/**", "/error", "/api/login/**"); // 로그인, 회원가입, 정적 자원, 에러 페이지는 예외
+                .excludePathPatterns("/sign_in", "/sign_up", "/resources/**", "/error", "/api/login/**", "/health"); // 로그인, 회원가입, 정적 자원, 에러 페이지는 예외
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080") // 올바른 도메인 설정
+                .allowedOrigins("https://r2r.o-r.kr") // 올바른 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
